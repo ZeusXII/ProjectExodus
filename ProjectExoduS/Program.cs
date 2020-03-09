9706ecb0a75fbc58485ba5456a1e7da7978fd1bc
ZeusXII <55351262+ZeusXII@users.noreply.github.com>
2020-03-09 10:14:40 +0100

    First push
    Rep established

---
+++ ProjectExoduS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectExoduS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
